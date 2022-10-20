761eecba3cc6218528525680b2f48a638c120a0b
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-10-20 16:57:23 -0300

    typing variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! It's my first code in Java Web!");
+        String nome = "Breno";
+        int idade = 18;
+        double altura = 1.80;
+        System.out.println("Olá, meu nome é " + nome + ", tenho " + idade + " anos e " + altura + " de altura.");
     }
 }
